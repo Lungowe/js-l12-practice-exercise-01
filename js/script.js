0f45e2ddd6b5c5773b2efeb88848837644736b8f
lungowe_s <lungowe.simasiku1@gmail.com>
2022-09-10 09:38:43 +0200

    Added the fetch command using async/await to grab random users from the randomUser API. Also included is the displayUsers function and the change event

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
 const randomFolks = document.querySelector(".random-peeps");
+const selectUserNumber = document.querySelector("select")
+
+async function getData(numUsers) {
+    const usersRequest = await fetch(`https://randomuser.me/api?results=${numUsers}`)
+    const data = await usersRequest.json()
+    const userResults = data.results
+    //console.log(userResults)
+    displayUsers(userResults)
+}
+
+getData()
+
+const displayUsers = (userResults) => {
+    randomFolks.innerHTML = " "
+    userResults.forEach(user => {
+        const country = user.location.country
+        const name = user.name.first
+        const imageURL = user.picture.medium
+        const userDiv = document.createElement('div')
+        userDiv.innerHTML = `
+            <h3>${name}</h3>
+            <p>${country}</p>
+            <img src=${imageURL} alt="User avatar" />`
+        randomFolks.append(userDiv)
+    })
+}
+
+selectUserNumber.addEventListener("change", event => {
+    const numUsers = event.target.value
+    getData(numUsers)
+})
+
